26e524cff9bb4226c226605312b2967ed29afe77
Jonathan Stone <jpstone@protonmail.com>
2019-04-25 01:30:13 -0500

    Added validations for citizenship and mailing.

--- client/src/Form.js
+++ client/src/Form.js
@@ -20,44 +20,70 @@ const initialState = {
 class Form extends React.Component {
   state = initialState;
 
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
-
   ageValidate = () => {
-    // age validator
     const parsedAge = parseInt(this.state.age, 10)
-    const onlyNumbers = /^\d+$/.test(this.state.age)
     const currentYear = new Date().getFullYear();
     let ageError = '';
-    if (!this.state.age === onlyNumbers) {
-      ageError = "User input must only be numbers"
+    if (!this.state.age) {
+      ageError = "Cannot be blank";
+    }
+    else if (!/^\d+$/.test(this.state.age)) {
+      ageError = "User input must only be numbers";
     }
     else if (this.state.age.length === 1 || this.state.age.length === 3) {
       ageError = "User input must be two (age) or four (year of birth) characters";
     }
     else if (this.state.age.length === 2 && (parsedAge > 100 || parsedAge < 18)) {
-      ageError = "User cannot be more than 100 years old or less than 18 years old"
+      ageError = "User cannot be more than 100 years old or less than 18 years old";
     }
-    else if (this.state.age.length === 4 && (parsedAge < (currentYear - 100) || parsedAge > (currentYear - 18))) {
-      ageError = "User must be between 18 and 99 years old"
+    else if (this.state.age.length === 4 && 
+        (parsedAge < (currentYear - 100) || parsedAge > (currentYear - 18))) {
+      ageError = "User must be between 18 and 99 years old";
     }
 
     if (ageError) {
       this.setState({ ageError });
       return false;
     }
-    else {
-      this.setState(initialState);
+    return true;
+  }
+
+  citizenshipValidate = () => {
+    let citizenshipError = '';
+    console.log(this.state.citizenship)
+    if (!/^[a-zA-Z\s]*$/g.test(this.state.citizenship)) {
+      citizenshipError = "Cannot include special characters or numbers";
+    }
+    if (citizenshipError) {
+      this.setState({ citizenshipError });
+      return false;
     }
     return true;
   }
 
+  mailingStateValidate = () => {
+    let mailingStateError = "";
+    if (!/^[a-zA-Z\s]*$/g.test(this.state.mailingState)) {
+      mailingStateError = "Cannot include special characters or numbers";
+    }
+    if (mailingStateError) {
+      this.setState({ mailingStateError });
+      return false;
+    }
+    return true;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
   handleSubmit = event => {
     event.preventDefault();
-    const isValid = this.ageValidate();
+    const isValid = this.ageValidate() && 
+                    this.citizenshipValidate() &&
+                    this.mailingStateValidate();
     if (isValid) {
       console.log(this.state);
       // clear form
@@ -70,7 +96,7 @@ class Form extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Policy Maximum
-          <select name="maximumPolicy">
+          <select name="maximumPolicy" value={this.state.value} onChange={this.handleChange}>
             <option defaultValue="choose">Choose your policy maximum</option>
             <option value="50">50,000</option>
             <option value="100">100,000</option>
@@ -118,7 +144,7 @@ class Form extends React.Component {
         <label>
           Mailing State
           <input 
-            name="maximumPolicy"
+            name="mailingState"
             type="text" 
             placeholder="Choose State" 
             onChange={this.handleChange} 
